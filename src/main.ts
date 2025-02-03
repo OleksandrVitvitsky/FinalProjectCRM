e8697ba1d324ed3bc12ab80108f68683326f8e85
OleksandrVitvitsky <vitvitsky.oleksandr@gmail.com>
2025-02-03 22:39:21 +0200

    init commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
-import express, {Request, Response} from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+
+import { ApiError } from './errors/api-error';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
+// Handle errors
+app.use(
+  '*',
+  (err: ApiError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status || 500).json(err.message);
+  },
+);
 
+// Handle uncaught exceptions
+process.on('uncaughtException', (e) => {
+  console.error('uncaughtException', e.message, e.stack);
+  process.exit(1);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
